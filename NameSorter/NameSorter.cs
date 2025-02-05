071ea578d30c5f93ee971494cc68a2d93a014ecb
Casey Leupolu <casey.leupolu@xero.com>
2025-02-05 19:52:33 +1300

    add name sorter and name sorter test projects

---
+++ NameSorter/NameSorter.cs
@@ -0,0 +1,15 @@
+﻿namespace NameSorter
+{
+    public class NameSorter
+    {
+        public static IEnumerable<string> SortNames(IEnumerable<string> names)
+        {
+            return names;
+        }
+
+        public static void SaveSortedNames(IEnumerable<string> sortedNames)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
